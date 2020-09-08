242d0d96f93b926edef8336339c63d61b519c403
程玉兵 <chengyubing@hupu.com>
2020-09-08 21:39:08 +0800

    some update

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 var http = require('http');
+var express = require('express');
+var fs = require('fs');
+var event = require('events');
 
 
 http.createServer(function(request, response) {
-    console.log(123123123)
-    // response.writeHead(200, {'Content-Type': 'text/plain'});
-
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello world');
 }) 
+fs.readFile('./test.txt', function(err, data) {
+    // console.log('sync data ===========>', data.toString());
+});
+var data2 = fs.readFileSync('./test.txt', 'utf-8');
 
-console.log('server is running at http://127.0.0.1:8888')
+var eventEmmiter = new event.EventEmitter();
+
+var connectHandler = function (){
+    console.log('触发连接事件');
+    eventEmmiter.emit('data_received');
+}
+
+eventEmmiter.on('connect', connectHandler);
+
+eventEmmiter.on('data_received', function(){
+    console.log('数据接收完成');
+})
+
+eventEmmiter.emit('connect');
+console.log('程序执行完成')
--- test1.js
+++
@@ -1,7 +0,0 @@
-console.log(this);
-var http = require('http')
-// console.log(a)
-// var path = require('path')
-// console.log(path);
-console.log(process.env.PATH);
-
